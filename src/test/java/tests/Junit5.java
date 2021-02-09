6458ec0878591009062d975dedf8b9e45fd9326f
zigl1 <ilgiz7725@gmail.com>
2021-02-08 20:00:37 -0500

    finish Homework(fixed)

--- src/test/java/tests/Junit5.java
+++ src/test/java/tests/Junit5.java
@@ -1,11 +1,13 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.support.FindBy;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Junit5 {
@@ -20,13 +22,15 @@ public class Junit5 {
         //Открыть страницу Selenide в Github
         open("https://github.com/selenide/selenide");
         //Перейти в раздел Wiki проекта
-        $x(".//span[contains(text(),'Wiki')]").click();
+        $(byText("Wiki")).click();
         //Убедится, что в списке страниц (Pages) есть страница SoftAssertions
         $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
         //Открыть страницу SoftAssertions
-        $x("//a[contains(text(),'SoftAssertions')]").click();
+        $(byText("SoftAssertions")).click();
         //проверить что внутри есть пример кода для JUnit5
         $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
     }
+
+
 
 }
